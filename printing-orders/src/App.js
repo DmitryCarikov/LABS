47dc9f681ef7647d489d521041e42da95081f145
Dmitry <93082934+DmitryCarikov@users.noreply.github.com>
2024-05-15 22:44:24 +0300

    feat: contruct the app

---
+++ printing-orders/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import OrdersPage from './pages/OrdersPage';
+import ClientsPage from './pages/ClientsPage';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/clients" element={<ClientsPage />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
