4a75021932634c3fdab5d2d7e325499fb2ed6514
Dmitry <93082934+DmitryCarikov@users.noreply.github.com>
2024-05-15 21:58:22 +0300

    feat: set API work in app component

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import ImageGallery from './components/ImageGallery';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: [],
+      query: '',
+      error: null
+    };
+  }
+
+  handleSearch = (query) => {
+    this.setState({ query }, this.fetchImages);
+  }
+
+  fetchImages = async () => {
+    const { query } = this.state;
+    if (!query.trim()) {
+      return;
+    }
+
+    const url = `https://api.unsplash.com/search/photos?query=${query}&client_id=lvtII6A4uFr_d5ANAMUE94gBw7BQ73uYsMGiO4m04yA`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+      const data = await response.json();
+      this.setState({ images: data.results, error: null });
+    } catch (error) {
+      this.setState({ error: error.message });
+    }
+  }
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  render() {
+    const { images, query, error } = this.state;
+    return (
+      <div>
+        <Header />
+        <SearchBar onSearch={this.handleSearch} query={query} />
+        {error && <div className="error">{error}</div>}
+        <ImageGallery images={images} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
